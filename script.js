bbfde2dd20c8cf9ac0ab4c2205fcee429fad056a
oxanadev <shana_bj@hotmail.com>
2024-07-25 22:37:07 +0500

    Added logo, About section

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-document.querySelectorAll("nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
 
 document.addEventListener("scroll", () => {
   const header = document.querySelector(".header-transparent");
 
   if (window.scrollY > 0) {
-    header.classList.add("header-gray");
+    header.classList.add("header-green");
   } else {
-    header.classList.remove("header-gray");
+    header.classList.remove("header-green");
   }
 });
