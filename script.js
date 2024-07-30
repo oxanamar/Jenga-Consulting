8620e16c9593c4273702509392a1fbb6e8e0a663
oxanadev <shana_bj@hotmail.com>
2024-07-30 22:31:20 +0500

    Added logo effect

--- script.js
+++ script.js
@@ -10,10 +10,15 @@
 
 document.addEventListener("scroll", () => {
   const header = document.querySelector(".header-transparent");
+  const logo = document.getElementById("logo");
 
   if (window.scrollY > 0) {
     header.classList.add("header-green");
+    logo.src = "./img/logo-only.png";
+    logo.classList.add("scrolled");
   } else {
     header.classList.remove("header-green");
+    logo.src = "./img/logo-words.png";
+    logo.classList.remove("scrolled");
   }
 });
